15456328184839754f726b52e48e70b5be83dfae
Batuhan Alkan <82328437+Wankelb@users.noreply.github.com>
2021-08-26 22:14:56 +0200

    Update tl_classifier.py

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -1,51 +1,29 @@
 from styx_msgs.msg import TrafficLight
 
-import cv2
 import numpy as np
 import tensorflow as tf
-import datetime
-
 
 import rospy
-from std_msgs.msg import String
-from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError
+
+
 
 class TLClassifier(object):
-    def __init__(self, is_simulation):
-        '''
-        self.image_pub = rospy.Publisher("image_topic_2",Image)
-        self.bridge = CvBridge()
-        '''
+    def __init__(self, model_path=None):
 
-        if is_simulation:
-            self.MODEL_NAME = 'light_classification/simulation'
-        else:
-            self.MODEL_NAME = 'light_classification/site'
+        self.graph = self.load_inference_graph(model_path)
+        self.sess = tf.Session(graph=self.graph)
 
-        self.PATH_TO_FROZEN_GRAPH = self.MODEL_NAME + '/inference_graph.pb'
 
-        #load classifier
-        # Load a (frozen) Tensorflow model into memory.
-        self.detection_graph = tf.Graph()
-        with self.detection_graph.as_default():
+    def load_inference_graph(self, path):
+        graph = tf.Graph()
+        with graph.as_default():
             od_graph_def = tf.GraphDef()
-            with tf.gfile.GFile(self.PATH_TO_FROZEN_GRAPH, 'rb') as fid:
+            with tf.gfile.GFile(path, 'rb') as fid:
                 serialized_graph = fid.read()
                 od_graph_def.ParseFromString(serialized_graph)
                 tf.import_graph_def(od_graph_def, name='')
-
-            self.image_tensor = self.detection_graph.get_tensor_by_name('image_tensor:0')
-            self.boxes = self.detection_graph.get_tensor_by_name('detection_boxes:0')
-            self.scores = self.detection_graph.get_tensor_by_name('detection_scores:0')
-            self.classes = self.detection_graph.get_tensor_by_name('detection_classes:0')
-            self.num_detections = self.detection_graph.get_tensor_by_name(
-                'num_detections:0')
- 
-        self.session = tf.Session(graph=self.detection_graph)
-        self.threshold = 0.5
-    
-
+        
+        return graph
     def get_classification(self, image):
         """Determines the color of the traffic light in the image
         Args:
@@ -53,38 +31,36 @@ class TLClassifier(object):
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
         """
+        #TODO implement light color prediction
+        np_image = self.load_image_into_numpy_array(image)
+        image_expanded = np.expand_dims(np_image, axis=0)
+        with self.graph.as_default():
+            #with tf.Session(graph=self.graph) as sess:
+            image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+            detect_boxes = self.graph.get_tensor_by_name('detection_boxes:0')
+            detect_scores = self.graph.get_tensor_by_name('detection_scores:0')
+            detect_classes = self.graph.get_tensor_by_name('detection_classes:0')
+            num_detections = self.graph.get_tensor_by_name('num_detections:0')
+            (boxes, scores, classes, num) = self.sess.run(
+                [detect_boxes, detect_scores, detect_classes, num_detections],
+                feed_dict={image_tensor: image_expanded})
+                
+                
+        return self.transform_idx(classes[0][0])
+    	
+    
+    def transform_idx(self,idx):
+        if idx==1:
+            return TrafficLight.GREEN
+        if idx==2: 
+            return TrafficLight.RED
+        if idx ==3:
+            return TrafficLight.YELLOW
+        if idx ==4:
+	    return TrafficLight.UNKNOWN
+	return TrafficLight.UNKNOWN
 
-        
-        #image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-        image_rgb = image
-
-        with self.detection_graph.as_default():
-            image_expand = np.expand_dims(image_rgb, axis=0)
-            start_classification_t = datetime.datetime.now()
-
-            (boxes, scores, classes, num_detections) = self.session.run(
-                [self.boxes, self.scores, self.classes, self.num_detections],
-                feed_dict={self.image_tensor: image_expand})
-
-            end_classification_t = datetime.datetime.now()
-            elapsed_time = end_classification_t - start_classification_t
-
-
-        boxes = np.squeeze(boxes)
-        scores = np.squeeze(scores)
-        classes = np.squeeze(classes).astype(np.int32)
-
-        
-
-        if scores[0] > self.threshold:
-            if classes[0] == 1:
-                print('Traffic Light is: GREEN')
-                return TrafficLight.GREEN
-            elif classes[0] == 2:
-                print('Traffic Light is: RED')
-                return TrafficLight.RED
-            elif classes[0] == 3:
-                print('Traffic Light is: YELLOW')
-                return TrafficLight.YELLOW
-
-        return TrafficLight.UNKNOWN
+    def load_image_into_numpy_array(self,image):
+ 
+        np_arr = np.asarray(image[:,:])
+        return np_arr
